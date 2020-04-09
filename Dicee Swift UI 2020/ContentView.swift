f41a2721b8ee6fbeec7af876fb82874908710417
Andrae J Henry <andrae.henry@icloud.com>
2020-04-09 09:05:35 -0400

    Final commit of Swift UI version of Dicee App

--- Dicee Swift UI 2020/ContentView.swift
+++ Dicee Swift UI 2020/ContentView.swift
@@ -9,8 +9,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+  @State  var leftDiceNumber = 1
+  @State  var rightDiceNumber = 2
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack{
+        Image("background")
+            .resizable()
+            .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("diceeLogo")
+                Spacer()
+                HStack{
+                    DiceView(n: leftDiceNumber)
+                    DiceView(n: rightDiceNumber)
+            }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("ROLL")
+                        .font(.system(size:50))
+                        .fontWeight((.heavy))
+                        .foregroundColor(.white)
+                }
+                .background(Color.red)
+            }
+    }
+    }
+}
+struct DiceView: View {
+    
+    let n : Int
+    var body: some View {
+        Image("dice\(n)")
+        .resizable()
+            .aspectRatio(contentMode: .fit)
+        .padding()
     }
 }
 
@@ -19,3 +57,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
